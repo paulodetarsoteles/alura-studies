2b403141413124fd7cfaea1e2a42bd1de5661f35
Paulo de Tarso <potassio@hotmail.com>
2024-02-12 20:19:17 -0300

    Criada a função que converte a string de tempo para segundos

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,7 +1,9 @@
+import { tempoParaSegundos } from "../../utils/date";
 import Botao from "../Botao";
 import Relogio from "./Relogio";
 
 function Cronometro() {
+    console.log("Teste de conversão: ", tempoParaSegundos("01:01:01"));
     return (
         <div style={
             {
